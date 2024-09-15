331a09bfbefd461525f2a10607b0303e2b8af566
Rusha Limbu <156543938+rkedem@users.noreply.github.com>
2024-09-15 17:46:44 -0400

    Added resources component

--- src/pages/resources.jsx
+++ src/pages/resources.jsx
@@ -4,9 +4,58 @@ import "../styles/resources.css";
 const Resources = () => {
     return(
         <div className="main">
-            <h1>Resources Page</h1>
+            <h1>CUNY</h1>
+            <h2>Resources</h2>
+        <p>
+            Students will have the opportunity to explore different career paths and access a variety of programs and resources aimed at enhancing their skills and acquiring additional knowledge beyond their academic pursuits. Click on any career path to begin.
+         </p> 
+        <div className="alphabet-bar">
+            <a href="#">A</a>
+            <a href="#">B</a>
+            <a href="#">C</a>
+            <a href="#">D</a>
+            <a href="#">E</a>
+            <a href="#">F</a>
+            <a href="#">G</a>
+            <a href="#">H</a>
+            <a href="#">I</a>
+            <a href="#">J</a>
+            <a href="#">K</a>
+            <a href="#">L</a>
+            <a href="#">M</a>
+            <a href="#">N</a>
+            <a href="#">O</a>
+            <a href="#">P</a>
+            <a href="#">Q</a>
+            <a href="#">R</a>
+            <a href="#">S</a>
+            <a href="#">T</a>
+            <a href="#">U</a>
+            <a href="#">V</a>
+            <a href="#">W</a>
+            <a href="#">X</a>
+            <a href="#">Y</a>
+            <a href="#">Z</a>
+        </div>
+
+        <div className="career-paths">
+                <h2>C</h2>
+                <u1>
+                    <li>Cybersecurity</li>
+                </u1>
+                <h2>F</h2>
+                <u1>
+                    <li>Front-end Developer</li>
+                </u1>
+                <h2>S</h2>
+                <u1>
+                    <li>Software Engineering</li>
+                </u1>
+
+        </div>
+
+        <a href="#" className="back-to-top">Back to Top</a>
         </div>
     );
 };
-
 export default Resources;
